eacd1e3da8ce7301e4d1833091ee78a48d3e371f
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-03-28 16:49:10 +0900

    111

--- Timer02/ViewController.swift
+++ Timer02/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 1, myTimer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named: "frame\(count).png")
+        countLabel.text = String(count)
     }
-
-
+    @IBAction func playBtnPressd(_ sender: Any) {
+        if(!myTimer.isValid) {
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+    }
+    }
+    @IBAction func stopBtnPressd(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @IBAction func resetBtnPressd(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+    }
+    @objc func doAnimation() {
+        count += 1
+        if count == 6 {
+            count = 1
+        }
+        myImageView.image = UIImage(named: "frame\(count).png")
+        countLabel.text = String(count)
+    }
 }
-
